0558fb97b45ae3bd4fe318069ad445ed77562b50
xmichaeld <xmichaeld@users.noreply.github.com>
2018-01-02 00:35:21 +0000

    20180102T003512Z

---
+++ cash.c
@@ -0,0 +1,42 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    float change;
+    int total = 0;
+    do
+    {
+        // ask for change
+        printf("How much change is owed?\n");
+        change = get_float();
+    }
+    // ensure that they give me a positive number of change
+    while (change < 0);
+    {
+        change = round(change*100);
+    }
+    // increase total for varying amounts of denominations of U.S coin currency
+    while (change >= 25)
+    {
+        change -= 25;
+        total++;
+    }
+    while (change >= 10)
+    {
+        change -= 10;
+        total++;
+    }
+    while (change >= 5)
+    {
+        change -= 5;
+        total++;
+    }
+    while (change >= 1)
+    {
+        change -= 1;
+        total++;
+    }
+    printf("%i\n", total);
+}
