a670c2e3d6784a93f08fb7f66dc1340bc07cc396
xmichaeld <xmichaeld@users.noreply.github.com>
2018-01-02 00:40:13 +0000

    20180102T003953Z

--- cash.c
+++ cash.c
@@ -15,24 +15,28 @@ int main(void)
     // ensure that they give me a positive number of change
     while (change < 0);
     {
-        change = round(change*100);
+        change = round(change * 100);
     }
     // increase total for varying amounts of denominations of U.S coin currency
+    // quarters
     while (change >= 25)
     {
         change -= 25;
         total++;
     }
+    // dimes
     while (change >= 10)
     {
         change -= 10;
         total++;
     }
+    // nickels
     while (change >= 5)
     {
         change -= 5;
         total++;
     }
+    // pennies
     while (change >= 1)
     {
         change -= 1;
